d7564f06ae00c70712044abd161f5d752d69c770
Antonio <tonijorda1997@gmail.com>
2020-12-15 15:14:18 +0300

    Display users with eloquent

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,48 +1,43 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
+            Hi.. <b> {{ Auth::user()->name }} </b>
+            <b style='float:right;'>Total Users
+            <span class=''>{{count($users)}}</span>
+        </b>
         </h2>
     </x-slot>
 
     <div class="py-12">
 
 
-<div class='container'>
-    <div class="row">
+        <div class='container'>
+            <div class="row">
 
-    <table class="table">
-  <thead>
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">First</th>
-      <th scope="col">Last</th>
-      <th scope="col">Handle</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td colspan="2">Larry the Bird</td>
-      <td>@twitter</td>
-    </tr>
-  </tbody>
-</table>
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">SL No</th>
+                            <th scope="col">Name</th>
+                            <th scope="col">Email</th>
+                            <th scope="col">Created At</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @php($i = 1)
+                        @foreach($users as $user)
+                        <tr>
+                            <th scope="row">{{$i++}}</th>
+                            <td>{{ $user->name }}</td>
+                            <td>{{$user->email}}</td>
+                            <td>{{$user->created_at->diffforHumans()}}</td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
 
-    </div>
-</div>
+            </div>
+        </div>
 
 
 
