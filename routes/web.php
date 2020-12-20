afb8cc162378952fe8b8ede056bbfff1a7aad94c
Antonio <tonijorda1997@gmail.com>
2020-12-20 16:22:31 +0300

    Split in components

--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,7 @@ Route::get('/email/verify', function () {
 
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
 Route::get('/home', function () {
     echo 'This is Home page';
